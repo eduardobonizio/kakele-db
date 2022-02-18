edd013f92a74249fbd8c9adb15e0bda30e2f1a62
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-18 09:44:34 -0300

    Remove a navbar e o footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
-import Footer from './pages/Footer/Footer';
-import NavBar from './pages/NavBar/NavBar';
 import Routes from './Routes/Routes';
 import './App.css';
 
 export default function App() {
-  return (
-    <>
-      <NavBar />
-      <div className="body-container">
-        <Routes />
-      </div>
-      <Footer />
-    </>
-  );
+  return <Routes />;
 }
