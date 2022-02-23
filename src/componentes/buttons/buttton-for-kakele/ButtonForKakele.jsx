d18096ae1995c5e51c609dad1a4c07c2e1cbb2e9
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-23 18:49:45 -0300

    Modifica para aceitar outros tipos de botão

--- src/componentes/buttons/buttton-for-kakele/ButtonForKakele.jsx
+++ src/componentes/buttons/buttton-for-kakele/ButtonForKakele.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 import styles from './ButtonForKakele.module.css';
 
 export default function ButtonForKakele(props) {
-  const { onClick, text } = props;
+  const { onClick, text, type } = props;
   return (
-    <button type="button" className={styles.button} onClick={() => onClick()}>
+    <button
+      type={type || 'button'}
+      className={styles.button}
+      onClick={() => onClick && onClick()}
+    >
       {text}
     </button>
   );
