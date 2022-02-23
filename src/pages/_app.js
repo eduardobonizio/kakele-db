18248e9dc454b344273bbc093e3c0b899cc6bdc2
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-23 12:38:25 -0300

    Importa o bootstrap js do modulo bootstrap via useEffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
 import { AppProvider } from '../componentes/useAppState';
 import Layout from '../componentes/Layout';
-import Script from 'next/script';
 import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('../../node_modules/bootstrap/dist/js/bootstrap.min.js');
+  }, []);
   return (
     <>
       <Head>
@@ -17,13 +20,6 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </Layout>
       </AppProvider>
-
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossOrigin="anonymous"
-        strategy="lazyOnload"
-      />
     </>
   );
 }
