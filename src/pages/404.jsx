5ef8877e9f6fdaa5a0dd1df881efa8a87fd2e274
Eduardo Bonizio <72473529+eduardobonizio@users.noreply.github.com>
2022-02-24 01:13:02 -0300

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+Import Head from "next/head"
 
 export default function NotFound() {
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center">
+      <Head>
+        <title>Kakele Tools - 404</title>
+        <meta
+          name="description"
+          content="Kakele Tools - Page Not Found"
+        />
+        <meta property="og:title" content="Kakele Tools - 404" key="title" />
+      </Head>
       <div>Página não encontrada</div>
     </div>
   );
