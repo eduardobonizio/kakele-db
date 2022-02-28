eb2808a32e5e2769541a7c7826ef26c99daaa79b
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-02-28 02:57:15 -0300

    Inicio dos posts

---
+++ _posts/posts.js
@@ -0,0 +1,18 @@
+[
+  {
+    titulo: 'Titulo do primeiro post',
+    intro: 'Primeiro parágrafo do primeiro post',
+    paragrafos: [
+      'Texto do primeiro parágrafo do primeiro post',
+      'Texto do segundo parágrafo do primeiro post',
+    ],
+  },
+  {
+    titulo: 'Titulo do segundo post',
+    intro: 'Primeiro parágrafo do segundo post',
+    paragrafos: [
+      'Texto do segundo parágrafo do segundo post',
+      'Texto do segundo parágrafo do segundo post',
+    ],
+  },
+];
