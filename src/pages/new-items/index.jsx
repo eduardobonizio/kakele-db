14943cd662c410df21369b4593e6a13c470de35a
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2022-03-06 23:50:51 -0300

    Adiciona visualização dos itens

--- src/pages/new-items/index.jsx
+++ src/pages/new-items/index.jsx
@@ -1,18 +1,21 @@
 import { useRouter } from 'next/router';
 import ItemCard from '../../componentes/others/item-card/ItemCard';
 import { NEW_ITEMS } from '../../data/kakeleData';
+import styles from './NewItems.module.css';
 
 const NewItemsPage = () => {
   const { locale, locales } = useRouter();
   const newItems = NEW_ITEMS;
   return (
-    <>
-      <div>
+    <div className={`container ${styles.mainContainer}`}>
+      <div className={`row row-cols-auto ${styles.row}`}>
         {newItems.map((item, index) => (
-          <ItemCard key={index} index={index} item={item} locale={locale} />
+          <div className={`col ${styles.col}`} key={item[locale]}>
+            <ItemCard index={index} item={item} locale={locale} />
+          </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
